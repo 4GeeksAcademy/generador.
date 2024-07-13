614875e4abb210dc61c3f9c38f0f6be06411a8c9
EuclidesT <94712951+EuclidesT@users.noreply.github.com>
2024-07-13 22:20:00 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(lista) {
+    let numAleatorio = Math.floor(Math.random() * lista.length);
+    return lista[numAleatorio] + " ";
+  }
+
+  let who = ["The Elephant", "My Cousin", "The Elf", "The Ghost"];
+  let action = ["took away", "hid", "broke", "threw away"];
+  let what = ["my umbrella", "my cookies", "the clock"];
+  let when = [
+    "before the party",
+    "while cooking",
+    "during the storm",
+    "when I was at the cinema"
+  ];
+
+  let excusa =
+    aleatorio(who) + aleatorio(action) + aleatorio(what) + aleatorio(when);
+
+  let elementExcuse = document.querySelector("#excuse");
+  elementExcuse.innerHTML = excusa;
+
+  let excuseButton = document.querySelector("#generate-excuse");
+
+  excuseButton.addEventListener("click", () => {
+    let nuevaExcusa =
+      aleatorio(who) + aleatorio(action) + aleatorio(what) + aleatorio(when);
+    elementExcuse.innerHTML = nuevaExcusa;
+  });
+  // document.querySelector("#excuse").innerHTML = excusa;
 };
